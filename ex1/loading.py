a936e92d56743a8e8155c026ad790b38e2e7817f
RocioMaSala <martinrocio16@gmail.com>
2026-05-28 17:11:19 +0200

    versión 28.5 tarde

--- ex1/loading.py
+++ ex1/loading.py
@@ -1,28 +1,59 @@
 import importlib.metadata
 import sys
 
-def check_imports() -> None:
-    try:
-        import numpy as np
-        import pandas as pd
-        import matplotlib.pyplot as plt
-    except ImportError as e:
-        print(e)
-        sys.exit(1) # Detiene la ejecución del script inmediatamente con un código de salida de error (1), impidiendo que el resto del programa intente correr sin las librerías necesaria
-
-def check_installed_packages():
+def check_dependencies() -> None:
     packages = ['pandas', 'numpy', 'matplotlib']
-    versions = {}
+    missing_packages = False
+
+    print("Checking dependencies:")
     for pkg in packages:
         try:
-            versions[pkg] = importlib.metadata.version(pkg)
-            print(f"[OK] {pkg} ({versions[pkg]}) - Ready to load")
+            version = importlib.metadata.version(pkg)
+
+            if pkg == "pandas":
+                msg = "Data manipulation ready"
+            elif pkg == "numpy":
+                msg = "Numerical computation ready"
+            elif pkg == "matplotlib":
+                msg = "Visualization ready"
+            else:
+                msg = "Ready to load"
+            print(f"[OK] {pkg} ({version}) - {msg}")
         except importlib.metadata.PackageNotFoundError:
-            versions[pkg] = "Not detected"
+            print (f"[ERROR] {pkg} - Not detected")
+            missing_packages = True
+
+    if missing_packages:
+        print("\nError: Missing dependencies.")
+        print("To install with pip: pip install -r requirements.txt")
+        print("To install with Poetry: poetry install")
+        sys.exit(1)
+
+def check_matrix_data() -> None:
+    
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    print("Analyzing Matrix data...")
+        
+    data = np.random.randn(500, 2)
+    frame = pd.DataFrame(data, columns=['X', 'Y'])
+    print(f"Processing {len(frame)} data points...")
+    print("Generating visualization...")
+
+    plt.scatter(frame['X'], frame['Y'], alpha=0.3, color='pink')
+    plt.title("Matrix Data Analysis")
+
+    plt.savefig("matrix_analysis.png")
+
+    print("\nAnalysis complete!")
+    print("Results saved to: matrix_analysis.png")
+
 
 if __name__ == "__main__":
     print("LOADING STATUS: Loading programs...\n")
-    check_imports()
-    print("Checking dependencies:")
-    check_installed_packages()
+    check_dependencies()
+    print("\n")
+    check_matrix_data()
 
